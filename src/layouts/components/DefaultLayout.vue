e9ddb1dae0ccafc2d729eb78b93c0c1668f5ac10
Mehmet Köl Tiğin <102043225+koltigin@users.noreply.github.com>
2024-04-14 17:39:53 +0300

    Update DefaultLayout.vue

--- src/layouts/components/DefaultLayout.vue
+++ src/layouts/components/DefaultLayout.vue
@@ -293,6 +293,17 @@ function selected(route: any, nav: NavLink) {
           >
             Telegram
           </div>
+        <a
+          href="https://cosmos-testnet.anatolianteam.com/"
+          target="_blank"
+          class="py-2 px-4 flex items-center rounded-lg cursor-pointer hover:bg-gray-100 dark:hover:bg-[#373f59]"
+        >
+          <Icon icon="mdi:web" class="text-xl mr-2" />
+          <div
+            class="text-base capitalize flex-1 text-gray-600 dark:text-gray-200"
+          >
+            Testnet Explorer
+          </div>
         </a>
       </div>
     </div>
